c745baa646974d06d84385f422932d58973416ed
Eli Oshinsky <eli@jcn.io>
2019-03-23 15:11:34 -0400

    fix endpoint url

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :discuss, Discuss.Web.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "polar-everglades-57320.herokuapp.com/", port: 443],
+  url: [host: "polar-everglades-57320.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
